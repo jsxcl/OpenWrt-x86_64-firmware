c2e72578d69afea5e77bfe0a43b7edadd6a552d7
jsxcl <candjay@sina.com>
2021-09-12 02:28:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cd package/lean
+git clone https://github.com/jerrykuku/luci-app-ttnode.git
+git clone https://github.com/fw876/helloworld.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+cd ..
+cd ..
